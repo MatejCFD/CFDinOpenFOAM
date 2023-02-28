a6fa1ff6db19e41fb1d5a23ac4fecbd9a1aa5172
MatejCFD <matej.tomic@gmail.com>
2023-02-28 10:59:09 +0100

    k-omega SST turbulence model cases

--- kOmeSST/extractCSV.sh
+++ kOmeSST/extractCSV.sh
@@ -3,9 +3,10 @@
 for i in {1..4}; do
     (
     cd kOme_config$i || continue
-    awk -F',' -v OFS=, 'NR>1 {print $2}'
     for j in 019 023 029 055; do 
-        kOme_config$i_$j.csv >> ../forOctave_kOme1095$i.ods
+        ( 
+        awk -F',' -v OFS=, 'NR>1 {print $2}' kOme_config$i_$j.csv >> ../forOctave_kOme1095$i.ods
+        )
     done
     )
 done
